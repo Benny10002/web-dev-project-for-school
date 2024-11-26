8caadbd482f3db04435f906c9d9cb607f931d507
Benny <71248398+Benny10002@users.noreply.github.com>
2024-11-26 09:28:59 +0000

    -

--- JS/index.js
+++ JS/index.js
@@ -7,4 +7,4 @@ scene.addButton(2, 3, "Button").find("button").on("click", (e) => {
     $(e.target).parent().parent().addClass("hide")
 })
 
-scene.addText("1 / 3", "1 / 3", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed placerat quis metus ut vulputate. Sed sodales nulla a turpis faucibus, sed elementum massa imperdiet. Sed nec nunc eleifend, blandit massa sed, tincidunt metus. Praesent at euismod felis, sed consectetur ante. Pellentesque venenatis metus leo.")
+scene.addText("1 / 2", "1 / 3", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed placerat quis metus ut vulputate. Sed sodales nulla a turpis faucibus, sed elementum massa imperdiet. Sed nec nunc eleifend, blandit massa sed, tincidunt metus. Praesent at euismod felis, sed consectetur ante. Pellentesque venenatis metus leo.")
