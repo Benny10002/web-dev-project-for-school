87d158847294d036dac40b2d7ced8ac1421a8f61
Orlando PC <orlando.l.schneider@gmail.com>
2024-12-06 20:37:38 +0100

    -

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,84 @@
 const scene = new Scene();
 
-scene.setGrid("25% ".repeat(4), "33% ".repeat(3));
+function divideEqualy(n, max = 100) {
+    return ((max / n).toString()+"%").repeat(n)
+}
 
-scene.addHeader(3, 2, "Hello world", "h2");
-scene.addButton(2, 3, "Button").find("button").on("click", (e) => {
-    $(e.target).parent().parent().addClass("hide")
-})
+console.log(divideEqualy(3))
 
-scene.addText("1 / 2", "1 / 3", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed placerat quis metus ut vulputate. Sed sodales nulla a turpis faucibus, sed elementum massa imperdiet. Sed nec nunc eleifend, blandit massa sed, tincidunt metus. Praesent at euismod felis, sed consectetur ante. Pellentesque venenatis metus leo.")
+function page1() { // swap page wrapper
+    scene.swapPage(page1_content)
+}
+
+function page1_content() {
+    scene.setGrid("10% "+divideEqualy(1, 80)+" 10%", "10% "+divideEqualy(1, 90) )
+
+    scene.addButton(3, 1, "Next").on("click", page2)
+
+    scene.addHeader(2, 2, "Page 1", "h2").parent().css("margin-top", "50%")
+}
+
+function page2() {
+    scene.swapPage(page2_content)
+}
+
+function page2_content() {
+    scene.setGrid("10% "+divideEqualy(1, 80)+" 10%", "10% "+divideEqualy(1, 90) )
+
+    scene.addButton(1, 1, "Previous").on("click", page1)
+    scene.addButton(3, 1, "Next").on("click", page3)
+
+    scene.addHeader(2, 2, "Page 2", "h2").parent().css("margin-top", "50%")
+}
+
+function page3() {
+    scene.swapPage(page3_content)
+}
+
+function page3_content() {
+    scene.setGrid("10% "+divideEqualy(1, 80)+" 10%", "10% "+divideEqualy(1, 90) )
+
+    scene.addButton(1, 1, "Previous").on("click", page2)
+    scene.addButton(3, 1, "Next").on("click", page4)
+
+    scene.addHeader(2, 2, "Page 3", "h2").parent().css("margin-top", "50%")
+}
+
+function page4() { 
+    scene.swapPage(page4_content)
+}
+
+function page4_content() {
+    scene.setGrid("10% "+divideEqualy(1, 80)+" 10%", "10% "+divideEqualy(1, 90) )
+
+    scene.addButton(1, 1, "Previous").on("click", page3)
+
+    scene.addHeader(2, 2, "Page 4", "h2").parent().css("margin-top", "50%")
+}
+
+
+function mainPage() {
+    scene.setGrid(divideEqualy(1), divideEqualy(5));
+    
+    scene.addSign(1, 4).append(
+        $("<div>")
+            .css({
+                "display": "flex",
+                "gap": "1vw",
+            })
+            .append($("<button>Page 1</button>").on("click", page1))
+            .append($("<button>Page 2</button>").on("click", page2))
+            .append($("<button>Page 3</button>").on("click", page3))
+            .append($("<button>Page 4</button>").on("click", page4))
+    )
+    scene.addSign(1, 3).append(
+        $("<h1>")
+            .css({
+                "font-size": "4em",
+            })
+            .html("Theater Fun Facts")
+    )
+    
+}
+
+mainPage()
--- JS/main.js
+++ JS/main.js
@@ -1,62 +1,115 @@
 class Scene {
     constructor() {
-        this.scene = $("<div class='scene'></div>");
-        this.grid = $("<div class='sceneGrid'></div>");
-        this.gridElements = [[]]
-        this.scene
-                .append($("<div class='box bgimg'></div>"))
-                .append($("<div class='box sceneContent'></div>")
-                    .append(
-                        $("<div class='sceneGridParent'></div>")
-                            .append(this.grid)
-                    ))
-                .append($("<div class='box floor'></div>"))
-                .append($("<div class='box ceiling'></div>"))
-                .append($("<div class='box leftwall'></div>"))
-                .append($("<div class='box rightwall'></div>"))
-        $("body").append(this.scene);
+/*
+ *  Theater
+ *      Scene
+ *          Wall Left
+ *          Wall Right
+ *          Background
+ *          Ceiling
+ *          Floor
+ *          Scene Content
+ *              Content Grid
+ *      Curtains parent
+ *          Curtains
+ *              Left
+ *              Right
+ *              Top
+*/
+
+// /*HTML*/ comments for vs code es6-string-html extension
+
+        this.theater = $(`<div class="theater nonInteractive"></div>`)
+        this.scene = $(/*HTML*/ 
+            `<div class="scene nonInteractive">
+                <div class="box bgimg nonInteractive"></div>
+                <div class="box floor nonInteractive"></div>
+                <div class="box ceiling nonInteractive"></div>
+                <div class="box leftwall nonInteractive"></div>
+                <div class="box rightwall nonInteractive"></div>
+            </div>`);
+        this.grid = {remove: ()=>{}}
+        this.theater.append(this.scene)
+        this.theater.append(
+            $(`<div class="foregroundCurtainsParent nonInteractive"></div>`)
+                .append(
+                    
+                    $( /*HTML*/ 
+                        `
+                        <div class="foregroundCurtains">
+                            <div class="curtain curtainLeft"></div>
+                            <div class="curtain curtainRight"></div>
+                            <div class="curtain curtainTop"></div>
+                            <div class="curtain curtainTopFill"></div>
+                        </div>`
+                    )
+                )
+        )
+        $(`body`).append(this.theater);
     }
 
     addElement(column, row) {
 
-        let e = $("<div class='grid_item'><div>")
+        let e = $(`<div class="grid_item"></div>`)
             .css("grid-column", column)
             .css("grid-row", row);
         this.grid.append(e);
         return e;
     }
-
-
+    
     setGrid(columns, rows) {
+        $(".sceneContent").remove()
+        this.grid = $(`<div class="contentGrid"></div>`);
+        this.scene.append(
+            $(`<div class="box sceneContent"></div>`)
+                .append(
+                    $(`<div class="sceneGridParent"></div>`)
+                        .append(this.grid)
+            )
+        )
         this.grid.css("grid-template-columns", columns);
         this.grid.css("grid-template-rows", rows);
+        setTimeout(()=>{
+            $(".curtain").removeClass("playCurtainClosed").css("left", "var(--leftOpen)")
+        }, 1500)
     }
-
+    
     addSign(column, row) {
-        let s = $("<div class='sign'></div>")
-        let strings = $("<div class='string'></div>")
-        let c = $("<div class='signContent'></div>")
+        let s = $(`<div class="sign"></div>`)
+        let strings = $(`<div class="string"></div>`)
+        let c = $(`<div class="signContent"></div>`)
         s.append(strings)
         s.append(c)
         s.addClass("show");
         this.addElement(column, row).append(s)
         return c
-
     }
-
+    
     addText(column, row, text) {
         return this.addSign(column, row)
-            .append($("<p></p>").html(text));
+        .append($(`<p></p>`).html(text));
     }
-
+    
     addHeader(column, row, text, headerSize) {
         return this.addSign(column, row)
-            .append($("<"+headerSize+"></"+headerSize+">").html(text));
+        .append($("<"+headerSize+"></"+headerSize+">").html(text));
     }
-
+    
     addButton(column, row, text) {
         return this.addSign(column, row)
-            .append($("<button></button>").html(text));
+        .append($(`<button></button>`).html(text));
+    }
+    
+    swapPage(page) {
+        $(".curtain").addClass("playCurtainClosed").css("left", "var(--leftClosed)")
+        $(".sign").addClass("hide")
+        setTimeout(()=>{
+            page();
+        }, 2000)
     }
 
+    removeSelf() {
+        this.theater.remove();
+    }    
+    
 }
